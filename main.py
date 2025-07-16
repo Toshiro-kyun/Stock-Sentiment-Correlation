3e69ea4679033ac6b04c19a599873dc1bbb9d01e
Toshiro-kyun <discordtoshiro@gmail.com>
2025-07-16 22:29:13 +0200

    Update to main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""Settings"""
+START_DATE = (datetime.now() - relativedelta(months=1)).strftime('%Y-%m-%d')
+END_DATE = datetime.now().strftime('%Y-%m-%d')
+
+STOCK = "NVDA"
+PAGES = 5
+MAX_LAG = 5
+
+"""Constants"""
+SEPERATOR = "-" * 200
+
 """Libaries"""
 # Standard libraries
 import os
@@ -9,7 +20,6 @@ import requests
 from dotenv import load_dotenv
 
 # Stock prices libraries
-import scipy.stats
 import yfinance as yf
 
 # NLP libaries
@@ -22,16 +32,18 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
 
 # Statistics libraries
-import scipy
-
-"""Settings"""
-START_DATE = (datetime.now() - relativedelta(months=1)).strftime('%Y-%m-%d')
-END_DATE = datetime.now().strftime('%Y-%m-%d')
-
-STOCK = "NVDA"
-PAGES = 5
+from scipy import stats
+from statsmodels.tsa.stattools import grangercausalitytests
 
 """Functions"""
+# Print pandas dataframe overview
+def pd_view(df: pd.DataFrame):
+    pd.set_option("display.max_rows", 10)
+    print(df)
+    pd.reset_option("display.max_rows")
+
+    print(SEPERATOR)
+
 # Fetch stock name and prices
 def fetch_stock_name_data(stock, start_date, end_date, verbose: bool =True):
     ticker = yf.Ticker(stock)
@@ -42,7 +54,7 @@ def fetch_stock_name_data(stock, start_date, end_date, verbose: bool =True):
     stock_data['Date'] = pd.to_datetime(stock_data["Date"]).dt.date
 
     if verbose:
-        print(stock_data.head())
+        pd_view(df=stock_data)
 
     return stock_name, stock_data
 
@@ -61,7 +73,6 @@ def fetch_stock_news(stock_name, start_date, end_date, pages: int, verbose: bool
         'sortBy': 'relevancy',
         'apiKey': api_key,
         'language': 'en',
-        'pageSize': 10
     }
 
     for page in range(1, pages):
@@ -86,8 +97,7 @@ def fetch_stock_news(stock_name, start_date, end_date, pages: int, verbose: bool
         news_data = pd.concat([news_data, new_news_data])
 
     if verbose:
-        print(news_data.head())
-        print(len(news_data))
+        pd_view(df=news_data)
 
     return news_data
 
@@ -95,6 +105,7 @@ def fetch_stock_news(stock_name, start_date, end_date, pages: int, verbose: bool
 def download_nltk():
     nltk.download('punkt_tab')
     nltk.download('stopwords')
+    print(SEPERATOR)
 
 # Preprocess sentence
 def preprocess_text(text, stop_words):
@@ -121,32 +132,103 @@ def sentiment_analysis(news_data, verbose: bool =True):
 
     # Perform sentiment analysis
     analyzer = SentimentIntensityAnalyzer()
-    news_data['Sentiment_score'] = news_data['Cleaned_headline'].apply(get_sentiment_score, args=(analyzer,))
+    news_data['Sentiment'] = news_data['Cleaned_headline'].apply(get_sentiment_score, args=(analyzer,))
     
     if verbose:
-        print(news_data.head())
+        pd_view(df= news_data)
 
     return news_data
 
 # Aggregate sentiments into pandas dataframe
-def aggregate_day_sentiment(news_data, verbose=True):
-    aggregated_news_data = news_data.groupby('Date')['Sentiment_score'].sum().reset_index()
+def aggregate_day_sentiment(news_data, verbose: bool =True):
+    aggregated_news_data = news_data.groupby('Date')['Sentiment'].sum().reset_index()
 
     if verbose:
-        print(aggregated_news_data.head())
+        pd_view(df= aggregated_news_data)
 
     return aggregated_news_data
 
 # Merge stock prices and sentiment scores
 def merge_data(stock_data, sentiment_data, verbose: bool =True):
-    merged_data = pd.merge(stock_data, sentiment_data, left_on="Date", right_on="Date", how="inner")
+    merged_data = pd.merge(stock_data, sentiment_data, left_on="Date", right_on="Date", how="outer")
 
     if verbose:
-        print(merged_data.head())
+        pd_view(df= merged_data)
 
     return merged_data
 
-def plot_graphs(combined_data, stock_name):
+# Employ time-lagged statistical methods
+def correlation_coef_statistics(stock_data, sentiment_data, stats_methods, max_lag: int, add_granger: bool = False, verbose: bool =True):
+    stats_data = pd.DataFrame()
+    stats_data["lag"] = range(-max_lag, max_lag + 1)
+
+    for name, method in stats_methods:
+        r_values, p_values = {}, {}
+        for lag in range(-max_lag, max_lag + 1):
+            shifted_stock_data = stock_data.shift(-lag)
+            valid = shifted_stock_data.notna() & sentiment_data.notna()
+            r_values[lag], p_values[lag] = method(sentiment_data[valid], shifted_stock_data[valid])
+        
+        stats_data[name + "--val"] = stats_data["lag"].map(r_values)
+        stats_data[name + "--pval"] = stats_data["lag"].map(p_values)
+
+    if add_granger:
+        r_values, p_values = {}, {}
+        granger_df = pd.DataFrame({"Stock_data": stock_data, "Sentiment_data": sentiment_data}).dropna()
+        granger_results = grangercausalitytests(granger_df, maxlag=max_lag, verbose=False)
+    
+        for lag in range(1, max_lag + 1):
+            ftest_result = granger_results[lag][0]['ssr_ftest']
+            r_values[lag] = ftest_result[0]
+            p_values[lag] = ftest_result[1]
+        
+        stats_data["Granger--val"] = stats_data["lag"].map(r_values)
+        stats_data["Granger--pval"] = stats_data["lag"].map(p_values)
+
+    if verbose:
+        pd_view(df= stats_data)
+
+    return stats_data
+
+# Find optimal lag from p-values
+def find_lag(stats_data):
+    pvals_df = stats_data[[column for column in stats_data.columns if column.endswith("--pval")]]
+    min_row_idx = pvals_df.min(axis=1).idxmin()
+    value = stats_data.loc[min_row_idx, 'lag']
+
+    return value
+
+# Plot statistical data
+def plot_stats_data(stats_data, stock_name):
+    fig, (subplt1, subplt2) = plt.subplots(1, 2)
+    for column in stats_data.columns:
+        if column == "lag":
+            continue
+        
+        if column.endswith("--val"):
+            subplt2.plot(stats_data["lag"], stats_data[column], label=column)
+        elif column.endswith("--pval"):
+            subplt1.plot(stats_data["lag"], stats_data[column], label=column)
+
+    subplt1.legend(loc="upper left")
+    subplt1.axhline(y=0.05, color="r", linestyle="--")
+    subplt1.set_title("P-values")
+    subplt2.legend(loc="upper left")
+    subplt2.set_title("Other values")
+    subplt2.axhline(y=0, color="r", linestyle="--")
+
+    fig.suptitle("Statisitcal Significance of Sentiment on Stock Price per Lag Interval")
+    plt.show()
+
+# Plot sentiment and stock price movement
+def plot_graphs(combined_data, stock_name, lag: int =0):
+
+    # Implement shift
+    combined_data = combined_data[["Date", "Close", "Sentiment"]].copy()
+    combined_data["Close"] = combined_data["Close"].shift(-lag)
+
+    combined_data = combined_data.dropna()
+
     # Visualize the data with a secondary y-axis and bar plot for aggregated sentiment scores
     fig, ax1 = plt.subplots(figsize=(14, 7))
 
@@ -163,22 +245,23 @@ def plot_graphs(combined_data, stock_name):
     ax2.set_ylabel('Aggregated Sentiment Score')
 
     # Use different colors for positive and negative bar values
-    colors = ['green' if val >= 0 else 'red' for val in combined_data['Sentiment_score']]
-    ax2.bar(combined_data['Date'], combined_data['Sentiment_score'], label='Aggregated Sentiment Score', color=colors, alpha=0.6)
+    colors = ['green' if val >= 0 else 'red' for val in combined_data['Sentiment']]
+    ax2.bar(combined_data['Date'], combined_data['Sentiment'], label='Aggregated Sentiment Score', color=colors, alpha=0.6)
     ax2.tick_params(axis='y')
 
-    ax2_deviation = combined_data["Sentiment_score"].max() - combined_data["Sentiment_score"].min()
-    ax2_mean = combined_data["Sentiment_score"].mean() 
+    ax2_deviation = combined_data["Sentiment"].max() - combined_data["Sentiment"].min()
+    ax2_mean = combined_data["Sentiment"].mean() 
     ax2.set_ylim(ax2_mean - ax2_deviation, ax2_mean + ax2_deviation)  # Set the right y-axis range
 
     fig.tight_layout()
-    plt.title(f'{stock_name} Stock Price vs Aggregated Sentiment Score')
+    plt.title(f'{stock_name} Stock Price vs Aggregated Sentiment Score - Lag = {lag}')
     fig.legend(loc='upper left', bbox_to_anchor=(0.1,0.9))
     plt.show()
 
 # Merge sentiment and 
 """Main Code"""
 if __name__ == "__main__":
+    print(SEPERATOR)
     # Load .env for news api-key
     load_dotenv()
 
@@ -197,13 +280,33 @@ if __name__ == "__main__":
     # Combined data
     combined_data = merge_data(stock_data=stock_data, sentiment_data=sentiment_data)
 
-    print(scipy.stats.spearmanr(combined_data["Close"].values, combined_data["Sentiment_score"].values)[0])
+    # Apply statistical techniques to find significance with time-lag
+    stats_methods = [
+        ("Spearmanr", stats.spearmanr),
+        ("Pearsonr", stats.pearsonr),
+        ("Kendaltau", stats.kendalltau)
+    ]
 
-    plt.scatter(combined_data['Sentiment_score'], combined_data['Close'])
-    plt.show()
+    stats_data = correlation_coef_statistics(stock_data=combined_data["Close"] , 
+                                              sentiment_data= combined_data["Sentiment"], 
+                                              stats_methods= stats_methods,
+                                              max_lag= MAX_LAG,
+                                              add_granger= False)
+    
+    #  Plot statistical data
+    plot_stats_data(stats_data=stats_data, stock_name= stock_name)
 
-    # Plot data
+    # Plot Stock Price and Sentiment over time
+
+    #   Without time-lag
     plot_graphs(combined_data=combined_data, stock_name=stock_name)
+
+    # Find the optimal lag
+    optimal_lag = find_lag(stats_data)
+
+    #   With optimized-time-lag
+    plot_graphs(combined_data=combined_data, stock_name=stock_name, lag=optimal_lag)
+
 
 
 
